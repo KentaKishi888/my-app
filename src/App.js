2cbb7b9d6d0b00292117de5a6b9e607cc943eded
KentaKishi888 <kenta.kishi@salto.link>
2022-08-21 16:01:37 +0900

    refactaring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const initialStates = {
+    name: "Netflix料金",
+    price: 1000,
+
+  }
+
+  const [state, setState] = useState(initialStates);
+  const [name, price] = state;
+
+  const reset = () => {
+    setState(initialStates);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setState({...state, price:price +1})}>+1</button>
+      <button onClick={() => setState({...state, price:price -1})}>-1</button>
+      <button onClick={reset}>reset</button>
+      <input value={state.name} onChange={(e)=> setState({...state, name:e.target.value})}/>
+  
     </div>
   );
 }
